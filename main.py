a54236a55ca265298de2471dd9afd6e073fb34e9
mattepuffo <matteo.ferrone@hotmail.it>
2021-10-26 17:52:47 +0200

    first

--- main.py
+++ main.py
@@ -3,20 +3,42 @@
 
 import tkinter as tk
 
+class MasterWindow:
+
+    def __init__(self, master=None):
+        self.master = master
+        master.title('MPQrcode')
+
+        w = 700
+        h = 700
+        ws = master.winfo_screenwidth()
+        hs = master.winfo_screenheight()
+        x = (ws / 2) - (w / 2)
+        y = (hs / 2) - (h / 2)
+        master.geometry("{}x{}+{}+{}".format(w, h, int(x), int(y)))
+
+        # LABEL
+        lblTitle = tk.Label(master, text="Scegli cosa inserire")
+        lblTitle.config(font=("Courier", 30))
+        lblTitle.grid(row=0, column=0, sticky='W', pady=20)
+
+        # RADIO
+        self.radioValue = tk.StringVar()
+        radioFoto = tk.Radiobutton(master, text='foto', value='foto', variable=self.radioValue)
+        radioFoto.grid(row=1, column=0, sticky='W', pady=20)
+        rdioUrl = tk.Radiobutton(master, text='url', value='url', variable=self.radioValue)
+        rdioUrl.grid(row=1, column=1, sticky='W', pady=20)
+
+        # BUTTON
+        button = tk.Button(text="Scegli!", width=25, height=5, command=self.buttonClick)
+        button.grid(row=2, column=0, sticky='W', pady=20)
+
+        master.resizable(0, 0)
+
+    def buttonClick(self):
+        print(self.radioValue)
+
 if __name__ == '__main__':
-    root = tk.Tk()
-    root.title('MPQrcode')
-
-    w = 700
-    h = 700
-    ws = root.winfo_screenwidth()
-    hs = root.winfo_screenheight()
-    x = (ws / 2) - (w / 2)
-    y = (hs / 2) - (h / 2)
-    root.geometry("{}x{}+{}+{}".format(w, h, int(x), int(y)))
-
-    lblTitle = tk.Label(root, text="Scegli cosa inserire")
-    lblTitle.grid(row=0, column=0, sticky='W', pady=2)
-
-    root.resizable(0, 0)
-    root.mainloop()
+    master = tk.Tk()
+    gui = MasterWindow(master)
+    master.mainloop()
