c6a792b585e525c2416fd3f7423c97732ec19251
mattepuffo <matteo.ferrone@hotmail.it>
2021-11-09 09:01:30 +0100

    class

--- main.py
+++ main.py
@@ -1,39 +1,48 @@
 # https://doc.qt.io/qtforpython/tutorials/basictutorial/uifiles.html
 
 import sys
+from pathlib import Path
 
 from PySide6.QtGui import Qt, QScreen
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QApplication, QWidget, QFileDialog
 from PySide6.QtCore import QFile, QCoreApplication
 from create_qrcode import CreateQrcode
 
-def btn_clicked(testo):
-    if testo:
-        cqr = CreateQrcode()
-        cqr.create('ciao')
-    else:
-        print('no')
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.uiFileName = "main.ui"
+        self.uiFile = QFile(self.uiFileName)
+
+        self.loader = QUiLoader()
+        self.window = self.loader.load(self.uiFile)
+        self.uiFile.close()
+
+        self.window.show()
+        self.center = QScreen.availableGeometry(QApplication.primaryScreen()).center()
+        self.geo = self.window.frameGeometry()
+        self.geo.moveCenter(self.center)
+        self.window.move(self.geo.topLeft())
+
+        self.window.txtTesto.setFocus()
+        self.window.btnCrea.clicked.connect(lambda: self.saveFileDialog(self.window.txtTesto.text().strip()))
+
+    def saveFileDialog(self, testo):
+        if testo:
+            cqr = CreateQrcode()
+            cqr.create('ciao')
+
+            options = QFileDialog.Options()
+            options |= QFileDialog.DontUseNativeDialog
+            fileName, _ = QFileDialog.getSaveFileName(self, "Salva", str(Path.home()),
+                                                      "All Files (*);;Text Files (*.txt)", options=options)
+            if fileName:
+                print(fileName)
 
 if __name__ == "__main__":
     QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
-
     app = QApplication(sys.argv)
-
-    uiFileName = "main.ui"
-    uiFile = QFile(uiFileName)
-
-    loader = QUiLoader()
-    window = loader.load(uiFile)
-    uiFile.close()
-
-    window.show()
-    center = QScreen.availableGeometry(QApplication.primaryScreen()).center()
-    geo = window.frameGeometry()
-    geo.moveCenter(center)
-    window.move(geo.topLeft())
-
-    window.txtTesto.setFocus()
-    window.btnCrea.clicked.connect(lambda: btn_clicked(window.txtTesto.text().strip()))
-
+    ex = MainWindow()
     sys.exit(app.exec())
